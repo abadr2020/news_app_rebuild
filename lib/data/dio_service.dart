555244833ff1608ac25c981976609eafd796cc9c
abadr2020 <ahmed.badr.khamis@gmail.com>
2025-12-30 11:23:42 +0200

    get news from one month earlier

--- lib/data/dio_service.dart
+++ lib/data/dio_service.dart
@@ -5,7 +5,7 @@ class DioService {
 
   Future<dynamic> getNews({required String searchitem}) async {
     final response = await dio.get(
-      'https://newsapi.org/v2/everything?q=$searchitem&from=2025-11-29&sortBy=publishedAt&apiKey=1f03e3fb988b484b9cc85ac560358dd7',
+      'https://newsapi.org/v2/everything?q=$searchitem&from=${DateTime.now().subtract(Duration(days: 30)).toIso8601String().split('T')[0]}&sortBy=publishedAt&apiKey=1f03e3fb988b484b9cc85ac560358dd7',
     );
     return response.data;
   }
